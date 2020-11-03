3aeaa6316681abfc11873e107aecd7ebdf1965cd
Jonatan Alonso <jonatancheuquepil98@gmail.com>
2020-11-03 14:54:12 -0300

    Base
    Esta es la platilla base, la cual cambiara.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# ForoSkin
-Es un skin de foro usando Vue y Vuetify
+# foroskin
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#A4FA22',
+        secondary: '#2E53D9',
+        accent: '#3E94F0',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      },
+    },
+  },
+});
